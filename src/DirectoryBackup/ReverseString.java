e31e6f98761e6db6619d061b672570031d762c84
Aman Raj <amanraj3809@gmail.com>
2023-12-27 21:22:51 +0530

    Create ReverseString.java

---
+++ src/DirectoryBackup/ReverseString.java
@@ -0,0 +1,17 @@
+public class ReverseString {
+  public static void main (String[] args) {
+       
+        String str= "test", nstr="";
+        char ch;
+       
+      System.out.print("Original word: ");
+      System.out.println(str); //Example word
+       
+      for (int i=0; i<str.length(); i++)
+      {
+        ch= str.charAt(i); //extracts each character
+        nstr= ch+nstr; //adds each character in front of the existing string
+      }
+      System.out.println("Reversed word: "+ nstr);
+    }
+}  
